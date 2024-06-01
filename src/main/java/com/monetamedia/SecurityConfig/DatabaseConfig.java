cf1c99603615ae019fb6e2d199d3e487dbd65095
Oloruntobi600 <tobiajayi60@gmail.com>
2024-06-01 21:08:36 +0100

    Dockerization of Api Endpoints

--- src/main/java/com/monetamedia/SecurityConfig/DatabaseConfig.java
+++ src/main/java/com/monetamedia/SecurityConfig/DatabaseConfig.java
@@ -1,5 +1,6 @@
 package com.monetamedia.SecurityConfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,6 +10,17 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String dbDriverClassName;
 
     @Bean
     public DataSource dataSource() {
@@ -16,6 +28,7 @@ public class DatabaseConfig {
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl("jdbc:postgresql://localhost:5433/Moneta_Media");
         dataSource.setUsername("postgres");
+//        dataSource.setPassword("newpassword");
         dataSource.setPassword("Olawumi");
         return dataSource;
     }
